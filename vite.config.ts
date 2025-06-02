e92a785d60c4f39628751705f9dcfb3ffe202969
Oneinfosys technologies <admin@oneinfosys.com>
2025-06-02 15:44:52 +0530

    Vite Update

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,10 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    hmr: {
+      overlay: false,
+      reload: false,
+    },
   },
   plugins: [
     react(),
